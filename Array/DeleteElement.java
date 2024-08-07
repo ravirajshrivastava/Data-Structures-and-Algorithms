f635b130854c306045b60736de406156d203c7cf
ravirajshrivastava <116421933+ravirajshrivastava@users.noreply.github.com>
2024-08-07 13:29:46 +0530

    Create DeleteElement.java

---
+++ Array/DeleteElement.java
@@ -0,0 +1,57 @@
+//Delete an element in array!!!
+
+
+package Array;
+
+import java.util.Scanner;
+
+public class DeleteElement
+{
+	static int elementDlt(int[] arr, int element, int n)
+	{
+		int i;
+		for(i=0; i<n; i++)
+		{
+			if(arr[i] == element)
+				break;
+		}
+		
+		if(i==n)
+		{
+			return n;
+		}
+		
+		for(int j=i; j<n-1; j++)
+		{
+			arr[j] = arr[j+1];
+		}
+		return (n-1);
+	}
+	
+	public static void main(String [] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the size of array: ");
+		int n = sc.nextInt();
+		
+		int[] arr = new int[n];
+		for(int i=0; i<n; i++)
+		{
+			arr[i] = sc.nextInt();	
+		}
+		for(int i=0; i<n; i++)
+		{
+			System.out.print(arr[i]+" ");	
+		}
+		
+		System.out.print("\nEnter the element to be deleted: ");
+		int element = sc.nextInt();
+		
+		n= elementDlt(arr, element, n);
+		
+		for(int i=0; i<n; i++)
+		{
+			System.out.print(arr[i]+" ");
+		}
+	}
+}
