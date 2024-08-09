bb91fda8892fb9cc678997683b4efbd1eb2b5ad7
ravirajshrivastava <116421933+ravirajshrivastava@users.noreply.github.com>
2024-08-09 23:18:01 +0530

    Add files via upload

---
+++ Array/DuplicateElements.java
@@ -0,0 +1,43 @@
+//Removing the Duplicate Elements using Naive Method that is taking a temporary array and copying the non- duplicate elements into that
+
+package Array;
+
+import java.util.Scanner;
+   
+public class DuplicateElements 
+{
+    void removeDupplicates(int[] arr, int[] temp)
+    {
+        int res=0;
+        for(int i=1; i<arr.length; i++)
+        {
+            if(arr[i] != arr[i-1])
+            {
+                temp[res] = arr[i];
+                res++; 
+            }
+        }
+        res = res+1;
+        System.out.println("Size of the array without duplicate elements is: "+res);
+    }
+     
+    public static void main(String[] args) {
+        DuplicateElements de = new DuplicateElements();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array");
+        int size = sc.nextInt();
+
+        int[] arr =new int[size];
+        int[] temp  = new int[size];
+
+        System.out.println("Enter the elements of the array");
+        for(int i=0; i<arr.length; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        // System.out.printf(de.removeDupplicates(arr, temp));
+        de.removeDupplicates(arr, temp);
+        
+    }
+}
