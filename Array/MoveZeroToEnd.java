df9c48cf45175c174a2d5294f22a56525e63e40b
ravirajshrivastava <116421933+ravirajshrivastava@users.noreply.github.com>
2024-08-10 02:24:40 +0530

    Add files via upload

---
+++ Array/MoveZeroToEnd.java
@@ -0,0 +1,48 @@
+//The code is to Move all the zeores to the end of the array 
+// This code is a Naive one
+
+package Array;
+
+public class MoveZeroToEnd 
+{
+
+    void swap(int[] arr, int i, int j)
+    {
+        int temp;
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    
+    void moveToEnd(int[] arr)
+    {
+        for(int i=0; i<arr.length; i++)
+        {
+            if(arr[i] == 0)
+            {
+                for(int j=i+1; j<arr.length; j++)
+                {
+                    if(arr[j] != 0)
+                    {
+                        swap(arr, i, j);
+                        
+                    }
+                }
+            }
+        }
+        System.out.print("Array is: ");
+        for(int i=0; i<arr.length; i++)
+        {
+            System.out.print(" "+arr[i]);
+        }
+    }
+    
+    public static void main(String[] args) {
+        MoveZeroToEnd mze = new MoveZeroToEnd();
+
+        int[] arr = {10,5,0,0,8,0,9,0};
+        // System.out.println("Length :"+arr.length);
+        mze.moveToEnd(arr);
+
+    }
+}
