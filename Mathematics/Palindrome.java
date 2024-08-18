f5af7465bd24519c0cb3e4b3369417872672e207
ravirajshrivastava <116421933+ravirajshrivastava@users.noreply.github.com>
2024-08-18 23:46:32 +0530

    Update Palindrome.java

--- Mathematics/Palindrome.java
+++ Mathematics/Palindrome.java
@@ -1,1 +1,40 @@
+// Check whether the number is plaindrome or not
+
+package Mathematics;
+
+import java.util.Scanner;
+
+public class Palindrome 
+{
+    void checkNumber(int num)
+    {
+        int res = 0;
+        int orgNum = num;
+        while(num > 0)
+        {
+            int rem = num%10;
+            res = rem + res*10;
+            num = num/10;
+        }
+
+        if(res == orgNum)
+        {
+            System.out.printf("Given number %d is palindrome. ",res);
+        }
+        else
+        {
+            System.out.println("Given number is not a palindrom number.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Palindrome pd = new Palindrome();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num = sc.nextInt();
+
+        pd.checkNumber(num);
+    }
+
+}
 
