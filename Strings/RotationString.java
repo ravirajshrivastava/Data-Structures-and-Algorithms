6d4c7a68d51d82423b0761ebb83feaad4fe705f1
ravirajshrivastava <116421933+ravirajshrivastava@users.noreply.github.com>
2024-08-23 16:07:01 +0530

    Add files via upload

---
+++ Strings/RotationString.java
@@ -0,0 +1,37 @@
+// Rotate the elements of the string one by be one and match with the given string whether it is the one of 
+// the rotated part of the original one or not
+
+package Strings;
+
+
+public class RotationString 
+{
+    String rotated;
+    boolean toCheck(String given, String check)
+    {
+        if (given.length() != check.length()) 
+        {
+            return false;
+        }
+        for(int i=0; i<given.length(); i++)
+        {
+            rotated = given.substring(i) + given.substring(0, i);
+
+            if(rotated.equals(check))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) 
+    {
+        String given = "Hello";
+        String check = "elloH";
+
+        RotationString rs = new RotationString();
+
+        System.out.println(rs.toCheck(given, check));
+
+    }    
+}
