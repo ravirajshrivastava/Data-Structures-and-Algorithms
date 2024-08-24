aed3ae83324ba4b7109f892230a6cc2a30a5e79b
Ravi Raj Shrivastava <shri.ravi0604@gmail.com>
2024-08-24 23:55:35 +0530

    Add files via upload

---
+++ Array/SubArrayNaive.java
@@ -0,0 +1,28 @@
+package Array;
+
+public class SubArrayNaive 
+{
+    void printArray(int[] arr)
+        {
+            for(int i=0; i<arr.length; i++)
+            {
+                for(int j=i; j<arr.length; j++)
+                {
+                    for(int k=i; k<=j; k++)
+                    {
+                        System.out.print(arr[k]+" ");
+                    }
+                    System.out.print(",");
+                }
+                System.out.print("\n");
+            }
+        }
+    public static void main(String[] args) 
+    {
+        SubArrayNaive sa = new SubArrayNaive();
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+
+        sa.printArray(arr);
+        
+    }    
+}
