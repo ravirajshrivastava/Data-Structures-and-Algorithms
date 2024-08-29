0fecd6db2f1416b22d7e7019abd54f3d990894da
Ravi Raj Shrivastava <shri.ravi0604@gmail.com>
2024-08-29 23:56:06 +0530

    Update UsingSortMethod.java

--- Sorting/UsingSortMethod.java
+++ Sorting/UsingSortMethod.java
@@ -1,4 +1,18 @@
-package Sorting;
+// It is just an example of how to use the Array.sort() method and what is its time complexity..
+// The time complexity of Arrays.sort() in Java depends on the type of array being sorted:
+
+// For Primitive Data Types (e.g., int[], char[]):
+
+// Time Complexity: O(n log n)
+// Algorithm Used: Dual-Pivot Quicksort, which is an optimized version of Quicksort.
+// For Objects (e.g., Integer[], String[]):
+
+// Time Complexity: O(n log n)
+// Algorithm Used: Timsort, which is a hybrid algorithm derived from Merge Sort and Insertion Sort.
+// In both cases, the average time complexity is O(n log n). However, Timsort is generally more efficient for partially sorted arrays or arrays with repeated elements, which is why it is used for object arrays.
+// package Sorting;
+
+
 
 import java.util.Arrays;
 
