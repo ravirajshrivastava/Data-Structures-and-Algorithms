bd9aa0fe576dbe0e005166290e9d38195fcd2595
ravirajshrivastava <shri.ravi0604@gmail.com>
2024-08-29 23:48:58 +0530

    initial commit

---
+++ UsingSortMethod.java
@@ -0,0 +1,18 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class UsingSortMethod
+{    
+    public static void main(String[] args) {
+        int[] arr = {13,3743,237,86,386,3961,849,36,87,68,276,183,1754};
+        
+        Arrays.sort(arr);
+        System.out.println(arr);
+        for(int i: arr)
+
+        {
+            System.out.print(i+" ");
+        }
+    }
+}
